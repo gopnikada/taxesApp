2ca53eb3f87b257f498382ca4804b553fb0c35a9
Kirill <klarsky.kirill6@gmail.com>
2022-12-18 20:01:11 +0100

    first commit

---
+++ TaxApp/TaxApp/Controllers/TaxesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TaxApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> GetTaxList()
+        {
+            return new List<string>() {"tax1", "tax2"};
+        }
+        [HttpGet("tax1")]
+        public IEnumerable<string> GetTax1()
+        {
+            return new List<string>() { "tax1 info.." };
+        }
+    }
+}
---
+++ TaxApp/TaxApp/Entities/Tax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxApp.Entities
+{
+    public class Tax
+    {
+        public byte Rating { get; set; }
+
+        public Argumentation Argumentation { get; set; }
+
+    }
+
+    
+}
